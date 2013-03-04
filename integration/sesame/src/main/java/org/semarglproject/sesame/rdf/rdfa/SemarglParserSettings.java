79d7916b5f065b4810a22e82aa5e8240e12b7547
Peter Ansell <p_ansell@yahoo.com>
2013-03-04 13:56:32 +1000

    Remove redundant settings in SemarglParserSettings

--- integration/sesame/src/main/java/org/semarglproject/sesame/rdf/rdfa/SemarglParserSettings.java
+++ integration/sesame/src/main/java/org/semarglproject/sesame/rdf/rdfa/SemarglParserSettings.java
@@ -15,15 +15,13 @@
  */
 package org.semarglproject.sesame.rdf.rdfa;
 
-import org.openrdf.rio.ParserSetting;
-import org.openrdf.rio.helpers.ParserSettingImpl;
+import org.openrdf.rio.RioSetting;
+import org.openrdf.rio.helpers.RioSettingImpl;
 import org.semarglproject.rdf.rdfa.RdfaParser;
-import org.semarglproject.source.StreamProcessor;
-import org.semarglproject.vocab.RDFa;
-import org.xml.sax.XMLReader;
 
 /**
- * Settings specific to Semargl that are not in {@link org.openrdf.rio.helpers.BasicParserSettings}.
+ * Settings specific to Semargl that are not in {@link org.openrdf.rio.helpers.BasicParserSettings}
+ * or {@link org.openrdf.rio.helpers.RDFaParserSettings} or {@link org.openrdf.rio.helpers.XMLParserSettings}.
  * 
  * @author Peter Ansell p_ansell@yahoo.com
  * @since 0.5
@@ -31,40 +29,14 @@ import org.xml.sax.XMLReader;
 public final class SemarglParserSettings {
 
     /**
-     * TODO: Javadoc this setting
+     * Enables or disables generation of triples from processor graph.
+     * ProcessorGraphHandler will receive events regardless of this option.
      * <p>
      * Defaults to false
      * @since 0.5
      */
-    public static final ParserSetting<Boolean> VOCAB_EXPANSION_ENABLED = new ParserSettingImpl<Boolean>(
-            RdfaParser.ENABLE_VOCAB_EXPANSION, "Vocabulary Expansion", Boolean.FALSE);
-
-    /**
-     * TODO: Javadoc this setting
-     * <p>
-     * Defaults to false
-     * @since 0.5
-     */
-    public static final ParserSetting<Boolean> PROCESSOR_GRAPH_ENABLED = new ParserSettingImpl<Boolean>(
-            RdfaParser.ENABLE_PROCESSOR_GRAPH, "Vocabulary Expansion", Boolean.FALSE);
-
-    /**
-     * TODO: Javadoc this setting
-     * <p>
-     * Defaults to 1.1
-     * @since 0.5
-     */
-    public static final ParserSetting<Short> RDFA_COMPATIBILITY = new ParserSettingImpl<Short>(
-            RdfaParser.RDFA_VERSION_PROPERTY, "RDFa Version Compatibility", RDFa.VERSION_11);
-
-    /**
-     * TODO: Javadoc this setting
-     * <p>
-     * Defaults to null
-     * @since 0.5
-     */
-    public static final ParserSetting<XMLReader> CUSTOM_XML_READER = new ParserSettingImpl<XMLReader>(
-            StreamProcessor.XML_READER_PROPERTY, "Custom XML Reader", null);
+    public static final RioSetting<Boolean> PROCESSOR_GRAPH_ENABLED = new RioSettingImpl<Boolean>(
+            RdfaParser.ENABLE_PROCESSOR_GRAPH, "Processor Graph Enabled", Boolean.FALSE);
 
     private SemarglParserSettings() {
     }
