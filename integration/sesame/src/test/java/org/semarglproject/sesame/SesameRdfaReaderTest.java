46396680cb248400e67083eb64c76eb44c275100
Peter Ansell <p_ansell@yahoo.com>
2013-05-23 12:40:09 +1000

    make verification errors non-fatal, as sesame is more strict in 2.7.0 by default
    also parse to a Model instead of just to a StatementCollector

--- integration/sesame/src/test/java/org/semarglproject/sesame/SesameRdfaReaderTest.java
+++ integration/sesame/src/test/java/org/semarglproject/sesame/SesameRdfaReaderTest.java
@@ -16,12 +16,16 @@
 package org.semarglproject.sesame;
 
 import org.openrdf.OpenRDFException;
+import org.openrdf.model.Model;
 import org.openrdf.model.Statement;
+import org.openrdf.model.impl.LinkedHashModel;
+import org.openrdf.rio.ParserConfig;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParser;
 import org.openrdf.rio.RDFWriter;
 import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.BasicParserSettings;
 import org.openrdf.rio.helpers.RDFaParserSettings;
 import org.openrdf.rio.helpers.RDFaVersion;
 import org.openrdf.rio.helpers.StatementCollector;
@@ -45,16 +49,20 @@ import static org.semarglproject.rdf.rdfa.RdfaTestSuiteHelper.runTestBundle;
 
 public class SesameRdfaReaderTest {
 
-    private StatementCollector model;
+    private Model model;
     private SaveToFileCallback sesameCallback = new SaveToFileCallback() {
         @Override
         public void run(Reader input, String inputUri, Writer output, short rdfaVersion) throws ParseException {
             try {
                 RDFParser rdfParser = Rio.createParser(RDFFormat.RDFA);
-                rdfParser.getParserConfig().set(RDFaParserSettings.RDFA_COMPATIBILITY, RDFaVersion.RDFA_1_1);
-                rdfParser.getParserConfig().set(RDFaParserSettings.VOCAB_EXPANSION_ENABLED, true);
-                rdfParser.getParserConfig().set(SemarglParserSettings.PROCESSOR_GRAPH_ENABLED, true);
-                rdfParser.setRDFHandler(model);
+                ParserConfig parserConfig = rdfParser.getParserConfig();
+                parserConfig.set(RDFaParserSettings.RDFA_COMPATIBILITY, RDFaVersion.RDFA_1_1);
+                parserConfig.set(RDFaParserSettings.VOCAB_EXPANSION_ENABLED, true);
+                parserConfig.set(SemarglParserSettings.PROCESSOR_GRAPH_ENABLED, true);
+                parserConfig.addNonFatalError(BasicParserSettings.VERIFY_DATATYPE_VALUES);
+                parserConfig.addNonFatalError(BasicParserSettings.VERIFY_LANGUAGE_TAGS);
+                parserConfig.addNonFatalError(BasicParserSettings.VERIFY_RELATIVE_URIS);
+                rdfParser.setRDFHandler(new StatementCollector(model));
                 rdfParser.parse(input, inputUri);
             } catch (OpenRDFException e) {
                 // do nothing
@@ -65,7 +73,7 @@ public class SesameRdfaReaderTest {
                 RDFWriter rdfWriter = Rio.createWriter(RDFFormat.TURTLE, output);
                 try {
                     rdfWriter.startRDF();
-                    for(Statement nextStatement : model.getStatements()) {
+                    for(Statement nextStatement : model) {
                         rdfWriter.handleStatement(nextStatement);
                     }
                     rdfWriter.endRDF();
@@ -83,7 +91,7 @@ public class SesameRdfaReaderTest {
 
     @BeforeClass
     public void init() throws SAXException, ClassNotFoundException {
-        model = new StatementCollector();
+        model = new LinkedHashModel();
     }
 
     @BeforeMethod
