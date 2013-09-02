f49d077e461c119871368fbb39968c9726059b75
Lev Khomich <levkhomich@gmail.com>
2013-09-02 17:53:35 +0600

    change SesameSink to be QuadSink

--- integration/sesame/src/main/java/org/semarglproject/sesame/core/sink/SesameSink.java
+++ integration/sesame/src/main/java/org/semarglproject/sesame/core/sink/SesameSink.java
@@ -24,6 +24,7 @@ import org.openrdf.model.impl.ValueFactoryImpl;
 import org.openrdf.rio.RDFHandler;
 import org.openrdf.rio.RDFHandlerException;
 import org.semarglproject.rdf.ParseException;
+import org.semarglproject.sink.QuadSink;
 import org.semarglproject.sink.TripleSink;
 import org.semarglproject.vocab.RDF;
 
@@ -41,7 +42,7 @@ import org.semarglproject.vocab.RDF;
  * @author Lev Khomich levkhomich@gmail.com
  *
  */
-public class SesameSink implements TripleSink {
+public class SesameSink implements QuadSink {
 
     /**
      * Used as a key with {@link #setProperty(String, Object)} method.
@@ -70,7 +71,7 @@ public class SesameSink implements TripleSink {
      * @param handler RDFHandler to sink triples to
      * @return new instance of Sesame sink
      */
-    public static TripleSink connect(RDFHandler handler) {
+    public static QuadSink connect(RDFHandler handler) {
         return new SesameSink(handler);
     }
 
@@ -112,6 +113,50 @@ public class SesameSink implements TripleSink {
     }
 
     @Override
+    public final void addNonLiteral(String subj, String pred, String obj, String graph) {
+        if (graph == null) {
+            addNonLiteral(subj, pred, obj);
+        } else {
+            addQuad(convertNonLiteral(subj), valueFactory.createURI(pred), convertNonLiteral(obj),
+                    convertNonLiteral(graph));
+        }
+    }
+
+    @Override
+    public final void addPlainLiteral(String subj, String pred, String content, String lang, String graph) {
+        if (graph == null) {
+            addPlainLiteral(subj, pred, content, lang);
+        } else {
+            if (lang == null) {
+                addQuad(convertNonLiteral(subj), valueFactory.createURI(pred), valueFactory.createLiteral(content),
+                        convertNonLiteral(graph));
+            } else {
+                addQuad(convertNonLiteral(subj), valueFactory.createURI(pred),
+                        valueFactory.createLiteral(content, lang), convertNonLiteral(graph));
+            }
+        }
+    }
+
+    @Override
+    public final void addTypedLiteral(String subj, String pred, String content, String type, String graph) {
+        if (graph == null) {
+            addTypedLiteral(subj, pred, content, type);
+        } else {
+            Literal literal = valueFactory.createLiteral(content, valueFactory.createURI(type));
+            addQuad(convertNonLiteral(subj), valueFactory.createURI(pred), literal, convertNonLiteral(graph));
+        }
+    }
+
+    protected void addQuad(Resource subject, URI predicate, Value object, Resource graph) {
+        try {
+            handler.handleStatement(valueFactory.createStatement(subject, predicate, object, graph));
+        } catch(RDFHandlerException e) {
+            // TODO: provide standard way to handle exceptions inside of triple sinks
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
     public void startStream() throws ParseException {
         try {
             handler.startRDF();
@@ -144,4 +189,5 @@ public class SesameSink implements TripleSink {
     @Override
     public void setBaseUri(String baseUri) {
     }
+
 }
